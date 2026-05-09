b54084c9cf4a6389b7eaabf9185dc93f26016fcc
Matthew Phillips <matthew@matthewphillips.info>
2026-05-09 15:32:58 -0400

    Bump version to 0.4.0

--- src/main.c
+++ src/main.c
@@ -7,7 +7,7 @@
 #include "agent.h"
 #include "util.h"
 
-#define ROSIE_VERSION "0.3.2"
+#define ROSIE_VERSION "0.4.0"
 
 static void print_usage(const char *prog) {
     printf("rosie - A robot helper for agent skills v%s\n\n", ROSIE_VERSION);
